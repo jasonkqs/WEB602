ea9338b58ae27cedc153e51d770252ca40bd8caa
Jason Kong <qkong@tesla.com>
2024-03-10 19:07:53 -0700

    :

---
+++ w1d3/lab/Readme.txt
@@ -0,0 +1,1 @@
+Week1 Day3 Lab assignment: Node.js and express
---
+++ w1d3/lab/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/index');
+const bodyParser = require('body-parser');
+const app = express();
+app.set('views',path.join(__dirname, 'views'));
+app.set('views',path.join('view engine', 'pug'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/', routes);
+module.exports=app;
